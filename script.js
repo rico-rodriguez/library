0e179a38154873b3c64cc18ed865239944617529
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-03-05 00:34:32 +0900

    Heavy progression
    Got all functions working properly. Books print to DIV's. Will add photo functionality and possibly backend and registration support.

--- script.js
+++ script.js
@@ -21,6 +21,47 @@ class Book {
 
     console.log(book1.sayResult())
 
-function addBooks() {
-  
+    // Get info from HTML form    
+let title
+let author
+let pages
+let bookBtn
+let read
+    bookBtn = document.getElementById("submit")
+    bookBtn.addEventListener('click', getFormInfo);
+function getFormInfo() {
+  console.log(document.getElementById("title").value)
+  title = document.getElementById("title").value;
+  author = document.getElementById("author").value;
+  pages = document.getElementById("pages").value;
+  read = document.querySelector('input[name="read"]:checked').value;
+
+  sendInfo()
+}
+
+// Send form info to a card and reset the info
+let tag
+let text
+let x
+function sendInfo() {
+  x = document.getElementById("title").value;
+  if (x == "") {
+      return false;
+  } else {
+  tag = document.createElement("div");
+  tag.className = 'relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 rounded-lg';
+
+  tag.innerHTML = 
+  `
+  <h4 id="book1" class="text-xl font-medium text-gray-700">
+  Book title: ${title} <br> 
+  Author name: ${author} <br>
+  Pages: ${pages} <br>
+  Read: ${read}
+  </h4>
+
+  `;
+  document.getElementById('bookList').appendChild(tag);
+  // document.getElementById("book1").innerHTML = `Book title: ${title}`;
+}
 }
