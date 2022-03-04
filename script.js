ab2e356e499768da9df93653f40b3cc28308418a
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-03-04 21:58:26 +0900

    Quick push to save layout.
    HTML is laid out. Time to organize and begin JS functions

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-let info
-  
-  class Book {
-    constructor(name, author, pages, read) {
+let myLibrary = [];
 
-        this.name = name
-        this.author = author
-        this.pages = pages
-        this.read = read
-
-        this.info = name + " by " + author + ", " + pages + " pages, " + "have been read."
-
-
-
-
+class Book {
+  constructor(bookName, author, pages, read) {
+    this.bookName = bookName
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+  sayResult() {
+    if (this.read === true) {
+      return console.log(`${this.bookName} by ${this.author}, ${this.pages} pages. Book has been read.`)
+    } else if (this.read === false) {
+      console.log(`${this.bookName} by ${this.author}, ${this.pages} pages. Book has NOT been read.`)
     }
+  }
 }
-  const book1 = new Book("PeterPan", "mikeJ", "293", "yes");
+  const book1 = new Book("PeterPan", "mikeJ", "293", false);
+  const book2 = new Book("James and the Giant Peach", "JG wentworth", "877", true);
 
-  console.log(book1.info)
+
+    console.log(book1.sayResult())
+
+function addBooks() {
   
+}
