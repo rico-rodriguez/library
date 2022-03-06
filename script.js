aff74f1516b86f58a8284c481f18804a889a5870
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-03-06 18:43:44 +0900

    Pushed card close update to JS elements

--- script.js
+++ script.js
@@ -69,8 +69,8 @@ function displayBook() {
   tag.innerHTML = 
   `
   <h4 id="bookCard" class="text-xl font-medium text-gray-700">
-  <span class="absolute top-0 bottom-0 right-3 px-4 py-3">
-  <svg type="button" onclick="" class="removeButton absolute fill-current h-4 w-4 text-black-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  <span class="absolute top-3 bottom-0 right-3 px-4 py-3">
+  <svg type="button" onclick="" class="cardClose removeButton fill-current h-7 w-7 text-black-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
 </span>
 <p class="text-center text-xl, font-bold">Book title:</p><hr><p class="text-center">${title}</p><hr>
 <p class="text-center text-xl, font-bold">Author name:</p><hr> <p class="text-center">${author}</p><hr>
