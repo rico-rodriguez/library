f6d1ca6c756d0ddf41b344a60ba95fb01444e2f8
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-03-06 13:54:55 +0900

    Visual and functional improvements
    Fixed the button for changing read status on dynamic elements.
    
    Updated Navigation link and added smooth scrolling to the link via jquery.

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 let myLibrary = [];
 class Book {
-  constructor(bookName, author, pages, read) {
-    this.bookName = bookName
+  constructor(title, author, pages, read) {
+    this.title = title
     this.author = author
     this.pages = pages
     this.read = read
   }
   sayResult() {
     if (this.read === true) {
-      return console.log(`${this.bookName} by ${this.author}, ${this.pages} pages. Book has been read.`)
+      return console.log(`${this.title} by ${this.author}, ${this.pages} pages. Book has been read.`)
     } else if (this.read === false) {
-      console.log(`${this.bookName} by ${this.author}, ${this.pages} pages. Book has NOT been read.`)
+      console.log(`${this.title} by ${this.author}, ${this.pages} pages. Book has NOT been read.`)
     }
   }
 }
-  const book1 = new Book("PeterPan", "mikeJ", "293", false);
-  const book2 = new Book("James and the Giant Peach", "JG wentworth", "877", true);
+  const Book0 = new Book("PeterPan", "mikeJ","293","No")
 
 
-    console.log(book1.sayResult())
+    console.log(Book0.sayResult())
 
     // Get info from HTML form    
 let title
@@ -57,31 +56,46 @@ function sendInfo() {
 } 
 //Push example book into Array
 myLibrary.push(["Call Sign Chaos", 'James Mattis', '1775', 'Yes']);
+
 function displayBook() {
   for (let i = 0; i < myLibrary.length; i++) { 
   //If form is filled out, create a div in the right section with Tailwind styling applied and variable text filled out.
-  tag = document.createElement("book" + (myLibrary.length)); }
+  tag = document.createElement("div"); 
+}
+
+
   tag.className = 
   'div relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 rounded-lg';
-
   tag.innerHTML = 
   `
-  <h4 id="book=" class="text-xl font-medium text-gray-700">
+  <h4 id="bookCard" class="text-xl font-medium text-gray-700">
   <span class="absolute top-0 bottom-0 right-3 px-4 py-3">
-  <svg type="button" class="removeButton absolute fill-current h-4 w-4 text-black-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  <svg type="button" onclick="" class="removeButton absolute fill-current h-4 w-4 text-black-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
 </span>
-  Book title: ${title} <br> 
-  Author name: ${author} <br>
-  Pages: ${pages} <br>
-  Read: ${read}
-  </h4>
-  `;
-  let newBook = document.getElementById('bookList').appendChild(tag);
-  myLibrary.push([title, author, pages, read]);
-  localStorage.setItem(newBook, myLibrary);
-  // image = document.getElementById('output');
+<p class="text-center text-xl, font-bold">Book title:</p><hr><p class="text-center">${title}</p><hr>
+<p class="text-center text-xl, font-bold">Author name:</p><hr> <p class="text-center">${author}</p><hr>
+<p class="text-center text-xl, font-bold">Pages:</p><p class="text-center">${pages}</p><hr>
+<p class="text-center text-center text-xl, font-bold">Read:</p><p class="changeRead text-center" id="changeRead">`
+if (read === "Yes") {
+  tag.innerHTML += `<button style="margin-top: 0px;" class="changeReadButton bg-blue-500 hover:bg-blue-700 text-white py-0 px-10 rounded font-medium text-xl m-0 mt-0">Yes</button></p>
+  </h4>`
+} else if (read ==="No") {
+  tag.innerHTML += `<button style="margin-top: 0px;" class="changeReadButton bg-blue-500 hover:bg-blue-700 text-white py-0 px-10 rounded font-medium text-xl m-0 mt-0">No</button></p>
+  </h4>`
+};
+  myLibrary.push([title, author, pages, read])
+  document.getElementById('bookList').appendChild(tag);
   alertBox.classList.add("hidden");
 }
+
+// --------------------------------------------------------
+//UTILITY FUNCTIONS BELOW THIS LINE -----------------------------------------------------------
+
+//Change read status on dynamicly created cards
+$(document).on("click", ".changeReadButton", function() {
+  const p = $(this).parent().children('.changeReadButton');
+    p.text(p.text() === 'Yes' ? 'No' : 'Yes')
+});
 
 //Closing the alert box
 function closeButton() {
@@ -94,13 +108,10 @@ $(document).on("click", ".removeButton", function() {
   $(this).closest(".div").remove();
 });
 
-
-// Upload Image
-
-// for (let i = 0; i < myLibrary.length; i++) { 
-// let loadFile = function(event) {
-// 	let image = document.getElementById('output');
-// 	image.src = URL.createObjectURL(event.target.files[0]);
-//   return image
-// };
-// }
+//Smooth scroll to anchor tag
+$('a').click(function(){
+  $('html, body').animate({
+      scrollTop: $( $(this).attr('href') ).offset().top
+  }, 1000);
+  return false;
+});
